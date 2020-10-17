acce3dbc6ac74fa96949780c50b500801c5cc778
tt-ubuntu <tim@teacherprints.org>
2020-10-17 13:17:53 -0700

    Embedded creation of local prodigy.json into script

--- get-datasets.sh
+++ get-datasets.sh
@@ -20,7 +20,7 @@ print_usage(){
   printf "${HELPFMT}" "-l" "LISTFILE" "filepath" "File in which to save the list of datasets"
   printf "${HELPFMT}" "-e" "EXPORT" "<none>" "Export the datasets to JSON files"
   printf "${HELPFMT}" "-s" "SUBSET" "int" "# of datasets to return (rather than all available)"
-  printf "${HELPFMT}" "-c" "CONSOLIDATIONFILE" "filepath" "File in which to dump consolidated dataset exports"
+  printf "${HELPFMT}" "-c" "CONSOLIDATIONFILE" "filepath" "File in which to dump consolidated dataset exports [not yet implemented]"
   printf "${HELPFMT}" "-h"  "<none>" "<none>" "Display help"
   printf "${HELPFMT}" "-r"  "<none>" "<none>" "Print references used in developing this script"
   delimit
@@ -42,7 +42,7 @@ while getopts 'hrd:l:es:c:' flag; do
     e) e_flag='true';
             EXPORT=true; printf " >> %-20s %s\n" "Exporting datasets?" "[${EXPORT}]" ;;
     s) s_flag='true';
-            SUBSET="${OPTARG}"; printf " >> %-20s %s\n" "Subsetting:" "[${SUBSET}] datasets" ;;
+            SUBSET="${OPTARG}"; printf " >> %-20s %s\n" "Subsetting:" "[${SUBSET}] dataset(s)" ;;
     c) c_flag='true';
             CONS="${OPTARG}"; printf " >> %-20s %s\n" "Consolidating datasets into file:" "[${CONS}]" ;;
     *) print_usage ;;
@@ -65,10 +65,22 @@ printf "${TASKFMT}" "...exporting to:" "[${DSLIST}]"
 
 delimit
 # echo "sqlite3 ${DB} \"${QUERY}\" >> ${DSLIST}"
-sqlite3 ${DB} "${QUERY}" >> ${DSLIST}
+sqlite3 ${DB} "${QUERY}" > ${DSLIST}
 
 if [[ $EXPORT == true ]];
-  then cat ${DSLIST} | xargs -I {} prodigy db-out {} ./
+  then cat << EOF > prodigy.json
+{
+"db": "sqlite",
+"db_settings": {
+  "sqlite": {
+    "name": "$(basename $DB)",
+    "path": "$(dirname $DB)"
+    }
+  }
+}
+EOF
+[ -d "./exports" ] && mkdir ./exports
+cat ${DSLIST} | xargs -I {} prodigy db-out {} ./exports
 fi
 
 if [ $REFERENCES ]; then
@@ -80,10 +92,19 @@ printf "%b\n" \
   "  -  https://jeredsutton.com/post/bash-bestish-practices-part-4/" \
   "  -  https://dev.to/rpalo/advanced-argument-handling-in-bash-377b" \
   "  -  https://stackoverflow.com/a/10973280" \
-  "  -  https://unix.stackexchange.com/questions/396223/bash-shell-script-output-alignment"
+  "  -  https://unix.stackexchange.com/questions/396223/bash-shell-script-output-alignment" \
+  "  -  https://stackoverflow.com/questions/5349718/how-can-i-repeat-a-character-in-bash" \
+  "  -  https://www.shellcheck.net" \
+  "  -  https://cameronnokes.com/blog/working-with-json-in-bash-using-jq" \
+  "  -  https://stackoverflow.com/questions/2664740/extract-file-basename-without-path-and-extension-in-bash" \
+  "  -  https://stackoverflow.com/a/22698106" \
+  "  -  https://stackoverflow.com/questions/6121091/get-file-directory-path-from-file-path" \
+  "And of course, the good folks at Explosion.ai (makers of Prodigy):" \
+  "  -  https://support.prodi.gy/t/specify-database-to-use-with-db-out/3427"
 delimit
 fi
 
+# TODO: Implement CONSOLIDATION functions
 # TODO: Implement check for prodigy executable in local env
 # TODO: Implement check for local prodigy.json; if not present, warn user (when verbose)
 # TODO: Implement dry-run equivalent for whole script
